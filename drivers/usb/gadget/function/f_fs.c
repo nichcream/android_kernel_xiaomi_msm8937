7dff498bbe55de794d15df9d99f2271823c04e34
hehao <hehao@xiaomi.com>
2016-12-14 20:42:09 +0800

    Kernel: Xiaomi kernel changes for Redme 3S
    The Patch based on QualComm release LA.UM.5.1-03810-8x37.0
    The kernel config file used is msm8937-perf_defconfig
    
    Change-Id: Ib2d2d410d350917479184123df5cd552f3000862
    Signed-off-by: hehao <hehao@xiaomi.com>
    
    Merge conflicts with LA.UM.5.6.r1-05900-89xx.0:
    	arch/arm/boot/dts/qcom/Makefile
    	arch/arm/boot/dts/qcom/apq8009w-swoctp.dtsi
    	arch/arm/boot/dts/qcom/apq8053-cdp.dts
    	arch/arm/boot/dts/qcom/apq8053-ext-audio-mtp.dts
    	arch/arm/boot/dts/qcom/mdmcalifornium-usb.dtsi
    	arch/arm/boot/dts/qcom/msm-pmi8950.dtsi
    	arch/arm/boot/dts/qcom/msm8909-pm8916-pm.dtsi
    	arch/arm/boot/dts/qcom/msm8917-camera.dtsi
    	arch/arm/boot/dts/qcom/msm8917.dtsi
    	arch/arm/boot/dts/qcom/msm8953-camera.dtsi
    	arch/arm/boot/dts/qcom/msm8953-cdp.dtsi
    	arch/arm/boot/dts/qcom/msm8953-ext-codec-mtp.dts
    	arch/arm/boot/dts/qcom/msm8953-mdss-panels.dtsi
    	arch/arm/boot/dts/qcom/msm8953-mdss-pll.dtsi
    	arch/arm/boot/dts/qcom/msm8953-mtp.dts
    	arch/arm/boot/dts/qcom/msm8953-mtp.dtsi
    	arch/arm/boot/dts/qcom/msm8953.dtsi
    	arch/arm/boot/dts/qcom/msmcobalt-coresight.dtsi
    	arch/arm/boot/dts/qcom/msmcobalt.dtsi
    	arch/arm/configs/msm8937-perf_defconfig
    	arch/arm/mm/context.c
    	arch/arm64/configs/msm8937_defconfig
    	arch/arm64/configs/msm_defconfig
    	arch/arm64/configs/msmcortex_defconfig
    	arch/arm64/configs/wt88537_defconfig
    	drivers/Kconfig
    	drivers/Makefile
    	drivers/char/diag/diag_masks.c
    	drivers/cpuidle/lpm-levels.c
    	drivers/gpu/msm/kgsl.c
    	drivers/gpu/msm/kgsl_sync.c
    	drivers/hwmon/qpnp-adc-common.c
    	drivers/input/touchscreen/Kconfig
    	drivers/leds/leds-qpnp-flash.c
    	drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
    	drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
    	drivers/media/radio/radio-iris-transport.c
    	drivers/media/radio/radio-iris.c
    	drivers/mmc/core/core.c
    	drivers/mmc/host/sdhci.c
    	drivers/power/qpnp-fg.c
    	drivers/power/qpnp-smbcharger.c
    	drivers/power/reset/msm-poweroff.c
    	drivers/scsi/ufs/ufs-debugfs.c
    	drivers/scsi/ufs/ufshcd.c
    	drivers/soc/qcom/Kconfig
    	drivers/soc/qcom/subsys-pil-tz.c
    	drivers/soc/qcom/subsystem_restart.c
    	drivers/usb/dwc3/debugfs.c
    	drivers/usb/gadget/function/f_serial.c
    	drivers/video/msm/mdss/mdp3_ctrl.c
    	drivers/video/msm/mdss/mdss_dsi.c
    	drivers/video/msm/mdss/mdss_fb.c
    	drivers/video/msm/mdss/mdss_mdp_rotator.c
    	drivers/video/msm/mdss/mdss_smmu.c
    	fs/fuse/shortcircuit.c
    	include/linux/usb/msm_hsusb.h
    	net/netfilter/xt_qtaguid.c
    	sound/soc/codecs/wcd-mbhc-v2.c
    	sound/soc/msm/msm8952.c
    	sound/soc/msm/qdsp6v2/q6asm.c
    
    Merge conflicts with LA.UM.6.6.r1-08600-89xx.0:
    	arch/arm64/configs/msm-perf_defconfig
    	arch/arm64/configs/msm_defconfig
    	arch/arm64/configs/wt88537-perf_defconfig
    	drivers/hwmon/qpnp-adc-common.c
    	drivers/leds/leds-qpnp-flash.c
    	drivers/power/qpnp-smbcharger.c
    	drivers/soc/qcom/Kconfig
    	drivers/video/msm/mdss/mdss_dsi.c
    	drivers/video/msm/mdss/mdss_dsi.h
    	drivers/video/msm/mdss/mdss_dsi_panel.c
    	drivers/video/msm/mdss/mdss_fb.c
    	sound/soc/codecs/msm8x16-wcd.c
    	sound/soc/msm/msm8952.c
    	include/linux/usb/msm_hsusb.h
    	include/uapi/linux/input.h (changes added to
    	include/uapi/linux/input-event-codes.h)

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -919,8 +919,7 @@ retry:
 
 			if (unlikely(ret < 0)) {
 				ret = -EIO;
-			} else if (unlikely(
-				   wait_for_completion_interruptible(done))) {
+			} else if (unlikely(wait_for_completion_interruptible_timeout(done, 10*HZ) <= 0)) {
 				spin_lock_irq(&epfile->ffs->eps_lock);
 				/*
 				 * While we were acquiring lock endpoint got
@@ -1612,6 +1611,9 @@ static void ffs_data_clear(struct ffs_data *ffs)
 			ffs->gadget, ffs->flags);
 
 	ffs_closed(ffs);
+
+	if (test_bit(FFS_FL_BOUND, &ffs->flags))
+		ffs_closed(ffs);
 
 	/* Dump ffs->gadget and ffs->flags */
 	if (ffs->gadget)
